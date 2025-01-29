179832ca3a837ac4d2f5cda682addb2729dacf59
Abdulaziz20040 <abduazizabdushukurov2004@gmail.com>
2025-01-29 21:38:45 +0500

    add api

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -1,6 +1,6 @@
 export const apiUrl = "https://a510c4f98367eca1.mokky.dev";
 export const shorts = `${apiUrl}/Shorts`;
-export const aniDubApi = `${apiUrl}/aniDub`;
+export const aniDubApi = `${apiUrl}/ANILIVE`;
 export const userDate = `${apiUrl}/userDate`;
 export const profilBacgrounimg = `${apiUrl}/profilBacgroundimg`;
 export const slider = `${apiUrl}/Slider`;
