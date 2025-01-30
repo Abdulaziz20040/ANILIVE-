707f0287b2acfb9e3f2fef645ceddf3a86f71347
Abdulaziz20040 <abduazizabdushukurov2004@gmail.com>
2025-01-30 12:23:54 +0500

    add slider respons 70%

--- src/pages/Home/Slider/Slider.jsx
+++ src/pages/Home/Slider/Slider.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "tailwindcss/tailwind.css";
-import "./slider.css";
 import {
   FaAngleRight,
   FaChevronLeft,
@@ -9,13 +8,11 @@ import {
   FaPlayCircle,
 } from "react-icons/fa";
 import Header from "../../../components/Header";
-import { IoTimeSharp } from "react-icons/io5";
-import { CiCalendarDate } from "react-icons/ci";
 import { slider } from "../../../Api/Api";
 import { Link } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import "../../../App.css";
+import "../../Home/Slider/slider.css";
 
 const Animation = () => {
   const [slides, setSlides] = useState([]);
@@ -26,8 +23,7 @@ const Animation = () => {
     axios
       .get(slider)
       .then((res) => {
-        const menu = res.data;
-        setSlides(menu || []);
+        setSlides(res.data || []);
         setLoading(false);
       })
       .catch((error) => {
@@ -67,19 +63,14 @@ const Animation = () => {
   if (slides.length === 0) {
     return (
       <div className="flex justify-center items-center h-screen bg-gray-200">
-        <Skeleton active paragraph={{ rows: 4 }} />
+        <Skeleton count={4} height={150} />
       </div>
     );
   }
 
   return (
-    <div className="">
-      <div
-        style={{
-          borderRadius: "0px 0px 7px 7px",
-        }}
-        className="relative sliderBorder w-full h-[55vh] sm:h-[67vh] md:h-[80vh] lg:h-[90vh] xl:h-[90vh] overflow-hidden"
-      >
+    <div>
+      <div className="relative w-full h-[55vh] sm:h-[67vh] md:h-[80vh] lg:h-[90vh] xl:h-[90vh] overflow-hidden">
         {/* Header */}
         <div className="absolute top-0 left-0 w-full z-30">
           <Header />
@@ -88,7 +79,7 @@ const Animation = () => {
         {slides.map((slide, index) => (
           <div
             key={index}
-            className={`absolute inset-0 bg-cover sliderSize bg-center transition-opacity duration-500 ease-in-out ${
+            className={`absolute inset-0 bg-cover bg-center transition-opacity duration-500 ease-in-out ${
               index === currentSlide ? "opacity-100" : "opacity-0"
             }`}
           >
@@ -100,60 +91,30 @@ const Animation = () => {
               }}
             ></div>
 
-            {/* Dark gradient on the left side */}
-            <div className="absolute inset-0 bg-gradient-to-r from-[#201f318f] to-transparent opacity-60"></div>
-
-            {/* Full Slide Darkness Overlay */}
-            <div
-              className="absolute inset-0 bg-[#201f31] opacity-40"
-              style={{ zIndex: "0" }}
-            ></div>
+            {/* Gradient Effect */}
+            <div className="absolute inset-0 bg-gradient-to-r from-black/90 via-black/70 to-transparent opacity-90"></div>
 
             {/* Slide Content */}
-            <div className="relative z-10 text-white px-4 md:px-12">
-              <div className="media">
-                <div className="widdf">
-                  <h3 className="titfd text-xl sm:text-2xl mb-4 md:text-3xl">
-                    {slide.name}
-                  </h3>
+            <div className="relative z-10 text-white SliderTop px-4 md:px-12">
+              <div>
+                <h3 className="mb-4 titfd line-clamp-2">{slide.name}</h3>
 
-                  {/* Date and Time (hidden on mobile) */}
-                  <div className="flex gap-8 mt-6 sm:mt-1 flex-wrap">
-                    <button className="flex items-center gap-2 text-sm sm:text-base ">
-                      <CiCalendarDate className="size-[20px]" />
-                      {slide.data}
+                {/* Description (hidden on mobile) */}
+                <p className="mt-4 subttile ">{slide.desc}</p>
+
+                {/* Watch Button */}
+                <div className="mt-4 flex items-center gap-4 flex-wrap">
+                  <Link to={`details/${slide.id}`}>
+                    <button className="flex items-center gap-2 watchBtn">
+                      <FaPlayCircle className="transition-transform duration-500 ease-in-out hover:rotate-360" />
+                      Ko'rish
                     </button>
-                    <button className="flex items-center gap-2 text-sm sm:text-base ">
-                      <FaPlayCircle />
-                      TV
-                    </button>
-                    <button className="flex items-center gap-1 text-sm sm:text-base ">
-                      <IoTimeSharp className="size-[19px] text-white" />
-                      {slide.time}
-                    </button>
-                  </div>
+                  </Link>
 
-                  <hr className="mt-4" />
-
-                  {/* Description (hidden on mobile) */}
-                  <p className="mt-4 hidden md:block font-normal text-white leading-6 tracking-normal subttile text-sm sm:text-base">
-                    {slide.desc}
-                  </p>
-
-                  {/* Watch Button */}
-                  <div className="mt-4 flex items-center gap-4 sm:gap-1 flex-wrap">
-                    <Link to={`details/${slide.id}`}>
-                      <button className="flex items-center gap-2 watchBtn">
-                        <FaPlayCircle className="transition-transform duration-500 ease-in-out hover:rotate-360" />
-                        Ko'rish
-                      </button>
-                    </Link>
-
-                    <button className="flex items-center sm:ms-0 ms-2 gap-2 bacgroountrans p-2 rounded-xl px-4 group">
-                      favorites
-                      <FaAngleRight className="group-hover:translate-x-2 transition-transform" />
-                    </button>
-                  </div>
+                  <button className="flex items-center gap-2 p-2 rounded-xl px-4 group bacgroountrans">
+                    favorites
+                    <FaAngleRight className="group-hover:translate-x-2 transition-transform" />
+                  </button>
                 </div>
               </div>
             </div>
@@ -161,23 +122,17 @@ const Animation = () => {
         ))}
 
         {/* Navigation Buttons */}
-        <div
-          style={{
-            flexDirection: "column",
-          }}
-          className="absolute bottom-4 right-4 flex gap-2 sm:gap-1 z-10"
-        >
-          {/* Visible only on medium and large screens */}
+        <div className="absolute bottom-4 right-4 flex gap-2 z-10">
           <div className="sm:flex hidden flex-col justify-center items-center gap-2">
             <button
               onClick={handlePrevSlide}
-              className="bg-gray-600  bg-opacity-70 text-white p-2 rounded-lg hover:bg-opacity-90"
+              className="bg-gray-600 bg-opacity-70 text-white p-2 rounded-lg hover:bg-opacity-90"
             >
               <FaChevronLeft size={20} />
             </button>
             <button
               onClick={handleNextSlide}
-              className="bg-gray-600  bg-opacity-70 text-white p-2 rounded-lg hover:bg-opacity-90"
+              className="bg-gray-600 bg-opacity-70 text-white p-2 rounded-lg hover:bg-opacity-90"
             >
               <FaChevronRight size={20} />
             </button>
